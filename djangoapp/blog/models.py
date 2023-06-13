686c090ebc6bc11a6736cc47cc0033bab80e6c38
João Victor <joaovictorsalotialves12@gmail.com>
2023-06-13 19:05:33 -0300

    Resize imagens from Posts

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from utils.rands import slugify_new
 from django.contrib.auth.models import User
+from utils.images import resize_image
 
 
 class Tag(models.Model):
@@ -121,6 +122,16 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify_new(self.title)
+
+        current_cover_name = str(self.cover.name)
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover_name != self.cover.name
+
+        if cover_changed:
+            resize_image(self.cover, 900, True, 70)
+
         return super().save(*args, **kwargs)
 
     def __str__(self) -> str:
