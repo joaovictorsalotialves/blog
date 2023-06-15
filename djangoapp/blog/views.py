4ad95a0b2cbfca68d0f35dc01d484c7cc69e2341
João Victor <joaovictorsalotialves12@gmail.com>
2023-06-15 11:37:45 -0300

    Configuring links within posts

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -6,7 +6,7 @@ PER_PAGE = 9
 
 
 def index(request):
-    posts = Post.objects.filter(is_published=True).order_by('-pk')
+    posts = Post.objects.get_published()
 
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
@@ -21,7 +21,7 @@ def index(request):
     )
 
 
-def page(request):
+def page(request, slug):
     return render(
         request,
         'blog/pages/page.html',
@@ -31,11 +31,45 @@ def page(request):
     )
 
 
-def post(request):
+def post(request, slug):
+    post = Post.objects.get_published().filter(slug=slug).first()
+
     return render(
         request,
         'blog/pages/post.html',
         {
-            # 'page_obj': page_obj,
+            'post': post
+        }
+    )
+
+
+def created_by(request, author_pk):
+    posts = Post.objects.get_published().filter(created_by__pk=author_pk)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(category__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
         }
     )
